a107e7d1c818568e1e84b77f2839787b5c63b4e2
hipl-yogitaprajapat <hipl.yogitaprajapat@gmail.com>
2025-06-11 15:38:41 +0530

    admin dashboard

--- backend/src/middleware/adminonly.middleware.js
+++
@@ -1,8 +0,0 @@
- // user is admin, allow access (only admin can add multiples users )
-export const adminOnly = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    return res.status(403).json({ message: "Access denied: only Admins can add users ", success: false });
-  }
-};
